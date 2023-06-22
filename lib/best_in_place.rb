1b63587e823dfa1850615c031e2b2a9df28ed2c4
Luciano Maiwald <mail@luciano-maiwald.de>
2023-06-22 13:35:50 +0200

    Do not autoload rails constants during initialization

--- lib/best_in_place.rb
+++ lib/best_in_place.rb
@@ -28,6 +28,5 @@ end
 require 'best_in_place/engine'
 require 'best_in_place/utils'
 require 'best_in_place/helper'
-require 'best_in_place/railtie'
 require 'best_in_place/controller_extensions'
 require 'best_in_place/display_methods'
